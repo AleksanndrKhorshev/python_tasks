0af5e6ebb6a247f7389ca6099f6413b3afa9bdb4
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-12-23 02:03:45 +0300

    task4

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -2,30 +2,17 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-import random
+from random import randint
 
-num = int(input("Ведите число: ")) #5
-my_list = []
-for i in range(num):
-    my_list.append(random.randint(-num, num))
-print(my_list)
+def list(n):
+    list = []
+    for i in range(n):
+        list.append(randint(-n, n))
+    return list
 
-path = 'file.txt'
-data = open(path, 'w')
-data.write('2\n')
-data.write('4\n')
-data.close()
-
-path = 'file.txt'
-data = open(path, 'r')
-mult = 1
-for line in data:
-    mult *= my_list[int(line)]
-print(mult)
-exit()
-multiplication = 1
-i = 1
-while i < len(my_list):
-    multiplication = my_list[i] * data[i]
-    i += 1
-print(multiplication)
+n = int(input('Введите число N: '))
+numbers = list(n)
+print(numbers)
+x = open('file.txt', 'r')
+result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+print(result)
